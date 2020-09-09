6a382145c12d9111ee11bdc012b4ed2dd8458877
tymoxx <7777kara@gmail.com>
2020-09-09 13:51:58 +0200

    Updates

---
+++ precache-manifest.ee48c891c26f6f65f4fd23bb5b6fdc48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a133853bba71abf7296396b8494b856",
+    "url": "/Magic-List-Of-Colors/index.html"
+  },
+  {
+    "revision": "5b055af7855fa38c664b",
+    "url": "/Magic-List-Of-Colors/static/css/main.a16910f3.chunk.css"
+  },
+  {
+    "revision": "b6fca8a731fcffbb483a",
+    "url": "/Magic-List-Of-Colors/static/js/2.41eeeb18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Magic-List-Of-Colors/static/js/2.41eeeb18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b055af7855fa38c664b",
+    "url": "/Magic-List-Of-Colors/static/js/main.71e0344f.chunk.js"
+  },
+  {
+    "revision": "2e511972da5ca4f8cdd6",
+    "url": "/Magic-List-Of-Colors/static/js/runtime-main.ebff5d24.js"
+  }
+]);
